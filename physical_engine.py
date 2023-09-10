cad9ac3edd91aefb2f9c377d906e2071ebd5f4c0
RoQBB <stepan.rojq@gmail.com>
2023-09-10 19:15:20 +0500

    (+) Объект с базовым объявленнием основных элементов движка

---
+++ physical_engine.py
@@ -0,0 +1,17 @@
+import arcade
+
+class RqPhysicalEngine:
+    """
+    Это физический движок игры.
+    Основная задача это реакция на происходящие вокруг игрока.
+    
+    """
+    def __init__(self, player):
+        pass
+
+    def add_script(self):
+        pass
+
+    def create_trigger(self):
+        pass
+
