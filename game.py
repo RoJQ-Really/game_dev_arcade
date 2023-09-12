de1cbe165df63dc6eb6471a168782507ba784c34
RoQBB <stepan.rojq@gmail.com>
2023-09-12 13:49:32 +0500

    (+) Базовый инициализатор движков (+) Поддержка курсора

---
+++ game.py
@@ -0,0 +1,27 @@
+import arcade
+import system
+
+
+class RqWindow(arcade.Window):
+    def __init__(self, physic_engine = None, world_engine = None, event_engine = None, ui_engine = None):
+        super().__init__(width=1280, height=720, title="")
+
+        self._system_engine = system.SystemEngine(self)
+        self.__init_engines()
+
+    def __init_engines(self):
+        self._system_engine.init_function()
+
+    def on_draw(self):
+        self.clear()
+        self._system_engine.on_draw()
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        self._system_engine.on_mouse_motion(x, y, dx, dy)
+
+    def update(self, delta_time: float):
+        self._system_engine.update()
+
+if __name__ == '__main__':
+    _window = RqWindow()
+    _window.run()
