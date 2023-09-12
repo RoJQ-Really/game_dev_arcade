9cc8eab427eee5a4af18aad92fadf0f82b120019
RoQBB <stepan.rojq@gmail.com>
2023-09-12 13:49:03 +0500

    (+) Абстрактный класс движков

---
+++ abstract.py
@@ -0,0 +1,16 @@
+
+
+class RqEngine:
+    def init_function(self, **kwargs):
+        """
+        Переопределяемая функция для инициализации движка
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def get_info(self):
+        """
+        Переопределяемяа функция для взаимодействия с окном игры
+        :return:
+        """
